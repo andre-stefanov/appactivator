dae9cff8ccbddb79664316edef836494c8c1ce9e
Andre Stefanov <andriy.stefanov@gmail.com>
2017-12-26 08:59:30 -0800

    travis

--- src/main/java/de/andrestefanov/appactivator/Application.java
+++ src/main/java/de/andrestefanov/appactivator/Application.java
@@ -8,8 +8,7 @@ import java.util.regex.Pattern;
 @SpringBootApplication
 public class Application {
 
-    static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
