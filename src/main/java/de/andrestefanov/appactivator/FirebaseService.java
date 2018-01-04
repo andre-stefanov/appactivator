6a33c26dba86f645b24a3bdd5775e752b8a6c611
Andre Stefanov <andre.stefanov@jambit.com>
2018-01-04 21:27:02 +0100

    fixed retrofit issue

--- src/main/java/de/andrestefanov/appactivator/FirebaseService.java
+++ src/main/java/de/andrestefanov/appactivator/FirebaseService.java
@@ -1,14 +1,14 @@
 package de.andrestefanov.appactivator;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface FirebaseService {
 
     @POST("https://fcm.googleapis.com/v1/projects/{project}/messages:send")
-    Call<Void> sendMessage(@Header("Authorization") String token, @PathVariable String project, @Body PushNotification content);
+    Call<Void> sendMessage(@Header("Authorization") String token, @Path("project") String project, @Body PushNotification content);
 
 }
